2589f8e33ecf8eb844a9b783fa520a5eeeec604f
hicoolasss <criopsdaun@gmail.com>
2023-09-08 10:32:10 +0300

    first commit

---
+++ registration.js
@@ -0,0 +1,7 @@
+var width = screen.width;
+var height = screen.height;
+
+document.getElementById("body").style.width = width;
+
+console.log("Размер видимой области (ширина): " + window.innerWidth + " пикселей");
+console.log("Размер видимой области (высота): " + window.innerHeight + " пикселей");
